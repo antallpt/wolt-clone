8928ebef39fd245aeaf4fab99c2b251dd79688ed
Antal Lepetit <antal.lpt@gmail.com>
2026-01-12 01:46:22 +0100

    List of categories and restaurants

--- app/(app)/(auth)/(tabs)/restaurants/index.tsx
+++ app/(app)/(auth)/(tabs)/restaurants/index.tsx
@@ -1,12 +1,1 @@
-import React from 'react'
-import { Text, View } from 'react-native'
-
-const Restaurants = () => {
-    return (
-        <View>
-            <Text>Restaurants</Text>
-        </View>
-    )
-}
-
-export default Restaurants
+export { default } from '@/components/screens/RestaurantListPage';
